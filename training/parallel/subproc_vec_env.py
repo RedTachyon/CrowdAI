23fb59887ac290f92c84dae190e2c34455d64d22
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2020-12-23 12:56:02 +0100

    Removed the automatic env resetting in subprocvecenv

--- training/parallel/subproc_vec_env.py
+++ training/parallel/subproc_vec_env.py
@@ -16,10 +16,10 @@ def _worker(remote, parent_remote, env_fn_wrapper):
             cmd, data = remote.recv()
             if cmd == 'step':
                 observation, reward, done, info = env.step(data)
-                if done:
-                    # save final observation where user can get it, then reset
-                    info['terminal_observation'] = observation
-                    observation = env.reset()
+                # if done:
+                #     # save final observation where user can get it, then reset
+                #     info['terminal_observation'] = observation
+                #     observation = env.reset()
                 remote.send((observation, reward, done, info))
             elif cmd == 'seed':
                 remote.send(env.seed(data))
