85087849e76cdaaae85c1db33d641997e0734cc6
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-11-18 02:16:16 +0100

    Actually add the animations

---
+++ Assets/BasicAnimation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnFootstep(AnimationEvent animationEvent)
+    {
+
+    }
+
+    private void OnLand(AnimationEvent animationEvent)
+    {
+
+    }
+}
