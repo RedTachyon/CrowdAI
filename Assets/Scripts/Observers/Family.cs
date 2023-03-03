d859e8aa7d709574bffb17944f6e9b1057c0e2db
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2023-03-03 11:38:41 +0100

    Updated family observer

--- Assets/Scripts/Observers/Family.cs
+++ Assets/Scripts/Observers/Family.cs
@@ -9,7 +9,7 @@ namespace Observers
     {
         public void Observe(VectorSensor sensor, Transform transform)
         {
-            // Identical as egocentric for now
+            // Identical as egocentric for now, but no goal?
             
             
             var agent = transform.GetComponent<AgentBasic>();
@@ -22,17 +22,17 @@ namespace Observers
             Vector3 goalPosition = goal.localPosition;
             
             // Position: 2
-            // sensor.AddObservation(position.x / 10f); // 1
-            // sensor.AddObservation(position.z / 10f); // 2
+            sensor.AddObservation(position.x / 10f); // 1
+            sensor.AddObservation(position.z / 10f); // 2
         
             // Rotation: 1
             sensor.AddObservation(rotation.eulerAngles.y / 360f); // 3
             
             // Relative position: 2
-            var relPosition = Quaternion.Inverse(rotation) * (goalPosition - agent.Family.GetPosition());
-            // var relPosition = goalPosition - position;
-            sensor.AddObservation(relPosition.x / 20f); // 4
-            sensor.AddObservation(relPosition.z / 20f); // 5
+            // var relPosition = Quaternion.Inverse(rotation) * (goalPosition - agent.Family.GetPosition());
+            // // var relPosition = goalPosition - position;
+            // sensor.AddObservation(relPosition.x / 20f); // 4
+            // sensor.AddObservation(relPosition.z / 20f); // 5
 
             // Debug.Log(relPosition);
             
@@ -49,14 +49,38 @@ namespace Observers
             
             sensor.AddObservation(agent.PreferredSpeed); // 12
             sensor.AddObservation(Manager.Instance.NormedTime); // 13
-            sensor.AddObservation(Manager.Instance.NormedTime); // 13
         }
         
-        public int Size => 13;
+        public int Size => 11;
 
         public float[] GetColliderInfo(Transform baseTransform, Collider collider)
         {
-            throw new System.NotImplementedException();
+            
+            var rigidbody = collider.GetComponent<Rigidbody>();
+            var transform = collider.transform;
+
+            var agent = collider.GetComponent<AgentBasic>();
+
+            var pos = transform.localPosition;
+            var velocity = rigidbody.velocity;
+
+            var rotation = baseTransform.localRotation;
+            pos = Quaternion.Inverse(rotation) * (pos - baseTransform.localPosition);
+            velocity = Quaternion.Inverse(rotation) * velocity;
+
+            var mass = agent == null ? 0f : agent.mass;
+            
+            float[] obs;
+
+  
+            obs = new[] { pos.x, pos.z, velocity.x, velocity.z, mass};
+
+            // if (baseTransform.name == "Person")
+            // {
+            //     Debug.Log($"{baseTransform.name} sees {transform.name} at {pos} with velocity {velocity} and mass {agent.mass}");
+            // }
+
+            return obs;
         }
     }
 }
